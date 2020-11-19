79ed0776855f39dbe681c686995191dc1e7bff27
Kim Beomhee <nakaldi@gmail.com>
2020-11-19 18:25:33 +0900

    Add epochtimesController to receive request as GET

---
+++ src/main/java/com/example/springtest/controller/epochtimesController.java
@@ -0,0 +1,18 @@
+package com.example.springtest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class epochtimesController {
+    @GetMapping("/items")
+    public Map getArgs(@RequestParam long from){
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("epochtimes", from);
+            return resultMap;
+    }
+}
