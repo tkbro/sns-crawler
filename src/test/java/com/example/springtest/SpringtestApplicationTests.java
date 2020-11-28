2f3edb51b8fca9b4d16a18cb503e9bc0bfa0c9d7
Kim Beomhee <nakaldi@gmail.com>
2020-11-29 01:48:42 +0900

    Add test code to test the operation of subList()

--- src/test/java/com/example/springtest/SpringtestApplicationTests.java
+++ src/test/java/com/example/springtest/SpringtestApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.springtest;
 
+import com.example.springtest.model.Article;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.springtest.repository.ArticleDataStore;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringtestApplicationTests {
+
+    @Autowired
+    ArticleDataStore articleDataStore;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+    void articleDataStoreTest() {
+        articleDataStore.put(1L, new Article("1", "url", "title", 1L));
+        articleDataStore.put(3L, new Article("1", "url", "title", 3L));
+        articleDataStore.put(2L, new Article("1", "url", "title", 2L));
+        articleDataStore.put(4L, new Article("1", "url", "title", 4L));
+
+        List<Article> articles = articleDataStore.subList(2L, 5L);
+        System.out.println(articles);
+
+        assertEquals(3, articles.size());
+        assertEquals(2L, articles.get(0).getEpochCreatedTime());
+        assertEquals(3L, articles.get(1).getEpochCreatedTime());
+        assertEquals(4L, articles.get(2).getEpochCreatedTime());
+    }
+
+    @Test
+    void articleDataStoreTest2() {
+        articleDataStore.put(1L, new Article("1", "url", "title", 1L));
+        articleDataStore.put(3L, new Article("1", "url", "title", 3L));
+        articleDataStore.put(2L, new Article("1", "url", "title", 2L));
+        articleDataStore.put(5L, new Article("1", "url", "title", 5L));
+
+        List<Article> articles = articleDataStore.subList(2L, 6L);
+        System.out.println(articles);
+
+        assertEquals(3, articles.size());
+        assertEquals(2L, articles.get(0).getEpochCreatedTime());
+        assertEquals(3L, articles.get(1).getEpochCreatedTime());
+        assertEquals(5L, articles.get(2).getEpochCreatedTime());
+    }
 }
