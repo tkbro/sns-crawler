7070f50a960b1035ff77476f3724c8c58fd43a93
Kim Beomhee <nakaldi@gmail.com>
2020-12-10 00:04:03 +0900

    Optimize imports

--- src/main/java/com/example/springtest/repository/ArticleRepository.java
+++ src/main/java/com/example/springtest/repository/ArticleRepository.java
@@ -3,9 +3,7 @@ package com.example.springtest.repository;
 import com.example.springtest.model.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.time.Instant;
 import java.util.List;
 
 @Repository
