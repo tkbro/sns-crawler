48320307b83125fe7d367abcd5530cdc8d2d586c
Kim Beomhee <nakaldi@gmail.com>
2020-12-09 23:58:34 +0900

    Fix to input the current time when there is no createdAt input

--- src/main/java/com/example/springtest/model/Article.java
+++ src/main/java/com/example/springtest/model/Article.java
@@ -2,12 +2,16 @@ package com.example.springtest.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
-public class Article extends Object {
+public class Article {
     private String id;
     private String url;
     private String title;
-    private long epochCreatedTime;
+    private Long createdAt = Instant.now().toEpochMilli();
 }
