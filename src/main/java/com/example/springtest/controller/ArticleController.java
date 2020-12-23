6501dc12ebfc9272ec36feeef76fd25f3ae8dcd4
Kim Beomhee <nakaldi@gmail.com>
2020-12-23 19:42:00 +0900

    Modify postArticles() to return ResponseEntity to respond with HTTP 201 CREATED

--- src/main/java/com/example/springtest/controller/ArticleController.java
+++ src/main/java/com/example/springtest/controller/ArticleController.java
@@ -3,11 +3,11 @@ package com.example.springtest.controller;
 import com.example.springtest.model.Article;
 import com.example.springtest.repository.ArticleMongoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class ArticleController {
@@ -21,10 +21,8 @@ public class ArticleController {
     }
 
     @PostMapping("/articles")
-    public Map<String, Object> postArticles(@RequestBody Article article) {
-        Map<String, Object> resultMap = new HashMap<>();
+    public ResponseEntity<Article> postArticles(@RequestBody Article article) {
         articleMongoRepository.save(article);
-        resultMap.put("article", article);
-        return resultMap;
+        return new ResponseEntity<>(article, HttpStatus.CREATED);
     }
 }
